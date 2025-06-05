6fd0a1c49caa6ff1fc0cd1a434acc16a851d2e39
Konstantin <89169555954@mail.ru>
2025-06-05 23:14:37 +0300

    новый проект

---
+++ src/test/java/ru/job4j/assertj/NameLoadTest.java
@@ -0,0 +1,52 @@
+package ru.job4j.assertj;
+
+import org.junit.jupiter.api.Test;
+import ru.job4j.assertj.NameLoad;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class NameLoadTest {
+    @Test
+    void checkEmpty() {
+        NameLoad nameLoad = new NameLoad();
+        assertThatThrownBy(nameLoad::getMap)
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessageContaining("no data");
+    }
+
+    @Test
+    void arrayIsEmpty() {
+        NameLoad nameLoad = new NameLoad();
+        assertThatThrownBy(nameLoad::parse)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Names array is empty");
+    }
+
+    @Test
+    void whenNameDoesNotContainEqualSigh() {
+        NameLoad nameLoad = new NameLoad();
+        String[] names = {"Alex"};
+        assertThatThrownBy(() -> nameLoad.parse(names))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("this name: Alex does not contain the symbol '='");
+
+    }
+
+    @Test
+    void whenNameStartsWithEqualSigh() {
+        NameLoad nameLoad = new NameLoad();
+        String[] names = {"=Alex"};
+        assertThatThrownBy(() -> nameLoad.parse(names))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("this name: =Alex does not contain a key");
+    }
+
+    @Test
+    void whenNameDoesNotContainValue() {
+        NameLoad nameLoad = new NameLoad();
+        String[] names = {"Alex="};
+        assertThatThrownBy(() -> nameLoad.parse(names))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("this name: Alex= does not contain a value");
+    }
+}
