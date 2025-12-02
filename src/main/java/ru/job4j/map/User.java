b04ce1edf27760e543ba397cd5124fdf964a23ed
Konstantin <89169555954@mail.ru>
2025-12-02 23:04:59 +0300

    новый проект

--- src/main/java/ru/job4j/map/User.java
+++ src/main/java/ru/job4j/map/User.java
@@ -1,4 +1,6 @@
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
 
 public class User {
 
@@ -10,5 +12,20 @@ public class User {
         this.name = name;
         this.children = children;
         this.birthday = birthday;
+    }
+
+    public static void main(String[] args) {
+
+        Calendar date = Calendar.getInstance();
+        date.set(1990, Calendar.JANUARY, 1);
+
+        User user1 = new User("Ivan", 2, date);
+        User user2 = new User("Ivan", 2, date);
+
+        Map<User, Object> map = new HashMap<>();
+        map.put(user1, new Object());
+        map.put(user2, new Object());
+
+        System.out.println(map);
     }
 }
