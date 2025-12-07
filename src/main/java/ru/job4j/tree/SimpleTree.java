24a17a89342bbe4034503110c6c43e07fd73a5f6
Konstantin <89169555954@mail.ru>
2025-12-07 23:20:06 +0300

    новый проект

--- src/main/java/ru/job4j/tree/SimpleTree.java
+++ src/main/java/ru/job4j/tree/SimpleTree.java
@@ -1,9 +1,11 @@
 package ru.job4j.tree;
 
-import java.util.*;
-import java.util.function.Predicate;
+import java.util.LinkedList;
+import java.util.Optional;
+import java.util.Queue;
 
 public class SimpleTree<E> implements Tree<E> {
+
     private final Node<E> root;
 
     public SimpleTree(final E root) {
@@ -13,36 +15,27 @@ public class SimpleTree<E> implements Tree<E> {
     @Override
     public boolean add(E parent, E child) {
         boolean rsl = false;
-        Optional<Node<E>> find = findBy(parent);
-        if (find.isPresent() && findBy(child).equals(Optional.empty())) {
-            Node<E> newNode = new Node<>(child);
-            find.get().children.add(newNode);
+        Optional<Node<E>> findParent = findBy(parent);
+        if (findParent.isPresent() && findBy(child).isEmpty()) {
+            findParent.get().children.add(new Node<>(child));
             rsl = true;
-        }
-        return rsl;
-    }
-
-    private Optional<Tree.Node<E>> findByPredicate(Predicate<Tree.Node<E>> condition) {
-        Optional<Node<E>> rsl = Optional.empty();
-        Queue<Node<E>> data = new LinkedList<>();
-        data.offer(this.root);
-        while (!data.isEmpty()) {
-            Node<E> el = data.poll();
-            if (condition.test(el)) {
-                rsl = Optional.of(el);
-                break;
-            }
-            data.addAll(el.children);
         }
         return rsl;
     }
 
     @Override
     public Optional<Node<E>> findBy(E value) {
-        return findByPredicate(el -> el.value.equals(value));
-    }
-
-    public boolean isBinary() {
-        return findByPredicate(el -> el.children.size() > 2).isEmpty();
+        Optional<Node<E>> rsl = Optional.empty();
+        Queue<Node<E>> data = new LinkedList<>();
+        data.offer(root);
+        while (!data.isEmpty()) {
+            Node<E> el = data.poll();
+            if (el.value.equals(value)) {
+                rsl = Optional.of(el);
+                break;
+            }
+            data.addAll(el.children);
+        }
+        return rsl;
     }
 }
