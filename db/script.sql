dbdae2113d8b4968fc93b3959ff75ef3b968bd18
Konstantin <89169555954@mail.ru>
2025-12-16 22:02:56 +0300

    новый проект

---
+++ db/script.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE job4j_db;
+
+CREATE TABLE cars (
+    id SERIAL PRIMARY KEY,
+    brand TEXT,
+    year INT,
+    electric BOOLEAN
+);
+
+INSERT INTO cars (brand, year, electric)
+VALUES ('Tesla', 2022, true);
+
+UPDATE cars
+SET year = 2023;
+
+DELETE FROM cars;
+E FROM cars;
