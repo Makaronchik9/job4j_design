5d4aa25a0024e5c56d9a926b46739cf9763df4f2
Konstantin <89169555954@mail.ru>
2026-01-04 13:55:13 +0300

    новый проект

---
+++ src/main/java/ru/job4j/ood/lsp/LspExample1.java
@@ -0,0 +1,22 @@
+package ru.job4j.ood.lsp;
+
+public class LspExample1 {
+
+    public static class Person {
+
+        public void breath() {
+        }
+
+        public void think() {
+        }
+
+        public void writeCode() {
+        }
+    }
+
+    public static class Dev extends Person {
+    }
+
+    public static class Doctor extends Person {
+    }
+}
---
+++ src/main/java/ru/job4j/ood/lsp/LspExample3.java
@@ -0,0 +1,22 @@
+package ru.job4j.ood.lsp;
+
+public class LspExample3 {
+
+    public static class Animal {
+
+        public void eat() {
+        }
+
+        public void move() {
+        }
+
+        public void fly() {
+        }
+    }
+
+    public static class Bird extends Animal {
+    }
+
+    public static class Cat extends Animal {
+    }
+}
