4c8d1e5c9b60efd9eb502d4a42ef12b56f86cf15
Konstantin <89169555954@mail.ru>
2026-01-21 22:22:13 +0300

    новый проект

---
+++ src/main/java/ru/job4j/ood/lsp/parking/AbstractParking.java
@@ -0,0 +1,86 @@
+package ru.job4j.ood.lsp.parking;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class AbstractParking implements Parking {
+
+    protected Car[] passengerCars;
+    protected Car[] truckCars;
+
+    public AbstractParking(int passengerCarCapacity, int truckCarCapacity) {
+        passengerCars = new Car[passengerCarCapacity];
+        truckCars = new Car[truckCarCapacity];
+    }
+
+    @Override
+    public void add(Car car) {
+        if ("passengerCar".equals(car.getType())) {
+            for (int i = 0; i < passengerCars.length; i++) {
+                if (passengerCars[i] == null) {
+                    passengerCars[i] = car;
+                    break;
+                }
+            }
+        } else if ("truckCar".equals(car.getType())) {
+            for (int i = 0; i < truckCars.length; i++) {
+                if (truckCars[i] == null) {
+                    truckCars[i] = car;
+                    return;
+                }
+            }
+            for (int i = 0; i <= passengerCars.length - car.getOccupiedCells(); i++) {
+                boolean gotPlace = true;
+                for (int j = 0; j < car.getOccupiedCells(); j++) {
+                    if (passengerCars[i + j] != null) {
+                        gotPlace = false;
+                        break;
+                    }
+                }
+                if (gotPlace) {
+                    for (int j = 0; j < car.getOccupiedCells(); j++) {
+                        passengerCars[i + j] = car;
+                    }
+                    return;
+                }
+            }
+        }
+    }
+
+    @Override
+    public void remove(Car car) {
+        if ("passengerCar".equals(car.getType())) {
+            for (int i = 0; i < passengerCars.length; i++) {
+                if (passengerCars[i] != null && passengerCars[i].equals(car)) {
+                    passengerCars[i] = null;
+                }
+            }
+        } else if ("truckCar".equals(car.getType())) {
+            for (int i = 0; i < truckCars.length; i++) {
+                if (truckCars[i] != null && truckCars[i].equals(car)) {
+                    truckCars[i] = null;
+                    return;
+                }
+            }
+            for (int i = 0; i < passengerCars.length; i++) {
+                if (passengerCars[i].equals(car)) {
+                    for (int j = 0; j < car.getOccupiedCells(); j++) {
+                        if (i + j < passengerCars.length) {
+                            passengerCars[i + j] = null;
+                        }
+                    }
+                    return;
+                }
+            }
+        }
+    }
+
+    @Override
+    public List<Car> findAll() {
+        List<Car> allCars = new ArrayList<>();
+        allCars.addAll(Arrays.asList(passengerCars));
+        allCars.addAll(Arrays.asList(truckCars));
+        return allCars;
+    }
+}
---
+++ src/test/java/ru/job4j/ood/lsp/parking/AbstractParkingTest.java
@@ -0,0 +1,151 @@
+package ru.job4j.ood.lsp.parking;
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class AbstractParkingTest {
+    private AbstractParking abstractParking;
+
+    @BeforeEach
+    void setUp() {
+        abstractParking = new AbstractParking(3, 2);
+    }
+
+    private boolean containsCar(AbstractParking parking, Car carToFind) {
+        return containsCarInArray(parking.passengerCars, carToFind) || containsCarInArray(parking.truckCars, carToFind);
+    }
+
+    private boolean containsCarInArray(Car[] cars, Car carToFind) {
+        boolean rsl = false;
+        for (Car car : cars) {
+            if (car != null && car.equals(carToFind)) {
+                rsl = true;
+                break;
+            }
+        }
+        return rsl;
+    }
+
+    @Test
+    public void whenPassengerCarIsAccepted() {
+        Car car = new Car("Audi", "passengerCar", 1);
+        Car car1 = new Car("BMW", "passengerCar", 1);
+        Car car2 = new Car("Toyota", "passengerCar", 1);
+        abstractParking.add(car);
+        abstractParking.add(car1);
+        abstractParking.add(car2);
+        assertTrue(containsCar(abstractParking, car));
+        assertTrue(containsCar(abstractParking, car1));
+        assertTrue(containsCar(abstractParking, car2));
+    }
+
+    @Test
+    public void whenPassengerCarIsNotAccepted() {
+        Car car = new Car("Audi", "passengerCar", 1);
+        Car car1 = new Car("BMW", "passengerCar", 1);
+        Car car2 = new Car("Toyota", "passengerCar", 1);
+        Car car3 = new Car("Fiat", "passengerCar", 1);
+        abstractParking.add(car);
+        abstractParking.add(car1);
+        abstractParking.add(car2);
+        assertTrue(containsCar(abstractParking, car));
+        assertTrue(containsCar(abstractParking, car1));
+        assertTrue(containsCar(abstractParking, car2));
+        assertFalse(containsCar(abstractParking, car3));
+    }
+
+    @Test
+    public void whenTruckCarIsAccepted() {
+        Car car = new Car("Kamaz", "truckCar", 2);
+        abstractParking.add(car);
+        assertTrue(containsCar(abstractParking, car));
+    }
+
+    @Test
+    public void whenTruckCarIsNotAccepted() {
+        Car car = new Car("Audi", "passengerCar", 1);
+        Car car1 = new Car("BMW", "passengerCar", 1);
+        Car car2 = new Car("Toyota", "passengerCar", 1);
+        Car car3 = new Car("Kamaz", "truckCar", 2);
+        Car car4 = new Car("Maz", "truckCar", 2);
+        Car car5 = new Car("Maz2", "truckCar", 2);
+        abstractParking.add(car);
+        abstractParking.add(car1);
+        abstractParking.add(car2);
+        abstractParking.add(car3);
+        abstractParking.add(car4);
+        abstractParking.add(car5);
+        assertTrue(containsCar(abstractParking, car));
+        assertTrue(containsCar(abstractParking, car1));
+        assertTrue(containsCar(abstractParking, car2));
+        assertTrue(containsCar(abstractParking, car3));
+        assertTrue(containsCar(abstractParking, car4));
+        assertFalse(containsCar(abstractParking, car5));
+    }
+
+    @Test
+    public void whenTruckCarIsAcceptedOnThePassengerCarsParking() {
+        Car car = new Car("Audi", "passengerCar", 1);
+        Car car1 = new Car("Kamaz", "truckCar", 2);
+        Car car2 = new Car("Maz", "truckCar", 2);
+        Car car3 = new Car("Maz2", "truckCar", 2);
+        abstractParking.add(car);
+        abstractParking.add(car1);
+        abstractParking.add(car2);
+        abstractParking.add(car3);
+        assertTrue(containsCar(abstractParking, car));
+        assertTrue(containsCar(abstractParking, car1));
+        assertTrue(containsCar(abstractParking, car2));
+        assertTrue(containsCar(abstractParking, car3));
+    }
+
+    @Test
+    public void whenRemovePassengerCar() {
+        Car car = new Car("Audi", "passengerCar", 1);
+        abstractParking.add(car);
+        abstractParking.remove(car);
+        assertFalse(containsCar(abstractParking, car));
+    }
+
+    @Test
+    public void whenRemoveTruckCar() {
+        Car car = new Car("Kamaz", "truckCar", 2);
+        abstractParking.add(car);
+        abstractParking.remove(car);
+        assertFalse(containsCar(abstractParking, car));
+    }
+
+    @Test
+    public void whenRemoveTruckCarFromPassengerSpots() {
+        Car car = new Car("Audi", "passengerCar", 1);
+        Car car1 = new Car("Kamaz", "truckCar", 2);
+        Car car2 = new Car("Maz", "truckCar", 2);
+        Car car3 = new Car("Maz2", "truckCar", 2);
+        abstractParking.add(car);
+        abstractParking.add(car1);
+        abstractParking.add(car2);
+        abstractParking.add(car3);
+        abstractParking.remove(car3);
+        assertTrue(containsCar(abstractParking, car));
+        assertTrue(containsCar(abstractParking, car1));
+        assertTrue(containsCar(abstractParking, car2));
+        assertFalse(containsCar(abstractParking, car3));
+    }
+
+    @Test
+    public void findAllCars() {
+        Car car = new Car("Audi", "passengerCar", 1);
+        Car car1 = new Car("Kamaz", "truckCar", 2);
+        abstractParking.add(car);
+        abstractParking.add(car1);
+        List<Car> allCars = abstractParking.findAll();
+        assertTrue(allCars.contains(car));
+        assertTrue(allCars.contains(car1));
+    }
+}
