205c732f0f1af98bb9dea43693d95d2b3c78379c
Konstantin <89169555954@mail.ru>
2026-01-22 22:49:11 +0300

    новый проект

---
+++ src/main/java/ru/job4j/ood/lsp/parking/SimpleParking.java
@@ -0,0 +1,8 @@
+package ru.job4j.ood.lsp.parking;
+
+public class SimpleParking extends AbstractParking {
+
+    public SimpleParking(int passengerCarCapacity, int truckCarCapacity) {
+        super(passengerCarCapacity, truckCarCapacity);
+    }
+}
