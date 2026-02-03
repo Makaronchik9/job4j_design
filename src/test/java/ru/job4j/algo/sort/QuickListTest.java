168486bd9c5db350f10ecc2cca08a82967d4b894
Konstantin <89169555954@mail.ru>
2026-02-03 15:40:52 +0300

    новый проект

--- src/test/java/ru/job4j/algo/sort/QuickListTest.java
+++ src/test/java/ru/job4j/algo/sort/QuickListTest.java
@@ -8,15 +8,53 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.*;
 class QuickListTest {
     @Test
-    void whenEmptyListThenOk() {
-        List<Integer> list = new ArrayList<>();
-        QuickList.quickSort(list, Integer::compareTo);
-        assertThat(list).isEmpty();
+    void whenSortThenOk() {
+        User user1 = new User(1, "11");
+        User user2 = new User(1, "22");
+        User user3 = new User(3, "33");
+        User user4 = new User(-4, "44");
+        User user5 = new User(4, "55");
+        User user6 = new User(6, "66");
+        Comparator<User> comparator = Comparator.comparingInt(User::getId);
+        List<User> listUser = new ArrayList<>();
+        listUser.add(user3);
+        listUser.add(user5);
+        listUser.add(user1);
+        listUser.add(user4);
+        listUser.add(user6);
+        listUser.add(user2);
+        QuickList.quickSort(listUser, comparator);
+        List<Integer> result = listUser.stream().map(User::getId).toList();
+        assertThat(result).containsExactly(-4, 1, 1, 3, 4, 6);
     }
 
     @Test
-    void whenOneElementThenOk() {
-        List<Integer> list = List.of(5);
-        QuickList.quickSort(new ArrayList<>(list), Integer::compareTo);
+    void whenReverseOrderThenOk() {
+        Comparator<Integer> comparator = (a, b) -> b - a;
+        List<Integer> list = new ArrayList<>();
+        list.add(-2);
+        list.add(-8);
+        list.add(1);
+        list.add(0);
+        list.add(10);
+        list.add(1);
+        list.add(8);
+        QuickList.quickSort(list, comparator);
+        System.out.println(list);
+        assertThat(list).containsExactly(10, 8, 1, 1, 0, -2, -8);
+    }
+
+    private static class User {
+        private Integer id;
+        private String name;
+
+        public User(int id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+
+        public int getId() {
+            return id;
+        }
     }
 }
